0434a21c5a1a124f3785052c4f5b016aa9e36361
salvadorgarrogomez <628626@alu.murciaeduca.es>
2024-03-20 12:09:39 +0000

    Modificacion vistas tree y form

--- helpdesk_salva/models/helpdesk_ticket.py
+++ helpdesk_salva/models/helpdesk_ticket.py
@@ -3,16 +3,38 @@ from odoo import fields,models
 class HelpdeskTicket(models.Model):
     _name = "helpdesk.ticket"
     _description = "Helpdesk Ticket"
+    _order = "sequence"
 
-    name = fields.Char()
+    name = fields.Char(
+        required = True,
+    )
     description = fields.Text()
-    date = fields.Date(help= "Date of the ticket")
-    limit_date = fields.Date(help= "Limit date of the ticket")
-    assigned = fields.Boolean(help = "Is the ticket assigned")
-    acctions_todo = fields.Html()
+    date = fields.Date(
+        help= "Date of the ticket"
+    )
+    limit_date = fields.Date(
+        help= "Limit date of the ticket"
+    )
+    assigned = fields.Boolean(
+        help = "Is the ticket assigned",
+        readonly = True,
+    )
+    actions_todo = fields.Html()
     user_id = fields.Many2one(
         comodel_name = 'res.users', 
-        string="Assigned to",
-        )
+        string="Assigned to")
+    sequence = fields.Integer()
+
+    state = fields.Selection(
+        [('nuevo', "Nuevo"),
+         ('asignado', "Asignado"),
+         ('en proceso', "En proceso"),
+         ('pendiente', "Pendiente"),
+         ('resuelto', "Resuelto"),
+         ('cancelado', "Cancelado")],
+         string = "State",
+         default = "nuevo",
+    )
+
 
 
